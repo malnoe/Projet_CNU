03eb1de3f6b011c362fd6eee2b5a10d80fdcfe68
Garance Malnoë <106530489+malnoe@users.noreply.github.com>
2024-11-04 18:40:48 +0100

    Squelette application

---
+++ app_projet.R
@@ -0,0 +1,59 @@
+library(shiny)
+library(bslib)
+library(markdown)
+
+ui <- navbarPage("App projet",
+                 # Page où le rapporteur peut étudier le dossier + émettre son avis.
+                 tabPanel("Avis du rapporteur",
+                          uiOutput("avisPanel")
+                 ),
+                 
+                 # Page contenant les statistiques sur les avis déjà émis.
+                 tabPanel("Statistiques",
+                          sidebarLayout(
+                            sidebarPanel("Bouttons pour la sélection des stats à afficher"),
+                            mainPanel("Graphiques / Informations statistiques liées aux candidatures")
+                          )
+                 ),
+                 
+                 # Page de connexion
+                 tabPanel("Connexion",
+                          card("Widget de connexion / déconnexion avec ID et mot de passe."),
+                          checkboxInput("checkConnecte", label = "Je suis connecté.e")
+                 )
+)
+
+server <- function(input, output, session) {
+  # Variable réactive pour l'état de connexion
+  connected <- reactive({ input$checkConnecte })
+  
+  # UI Page Avis rapporteur
+  output$avisPanel <- renderUI({
+    if (connected()) {
+      # Boutons et éléments pour l'avis
+      sidebarLayout(
+        sidebarPanel(
+          actionButton("buttonMenu", label = "Menu"),
+          actionButton("buttonSave", label = "Sauvegarder"),
+          checkboxInput("checkTermine", label = "J'ai terminé"),
+          selectInput("selectAvis", label = "Avis", choices = list("Sélectionner", "Très favorable", "Favorable", "Réservé"), selected = NULL),
+          checkboxGroupInput(
+            "checkGroupeModalites",
+            "Modalités d'attribution",
+            choices = list("M1 (Ens)" = 1, "M2 (Rech)" = 2, "M3 (OIP)" = 3, "M4 (Diff)" = 4, "M5 (Eur)" = 5, "M6 (Coop)" = 6, "M7 (Invest)" = 7)
+          ),
+          actionButton("buttonUploadPreRapport", label = "Déposer un pré-rapport (PDF)")
+        ),
+        mainPanel(
+          actionButton("buttonTelechargeDossier", label = "Télécharger le dossier du/de la candidat.e (PDF)"),
+          card("PDF ou texte comprenant les informations de la candidature")
+        )
+      )
+    } else {
+      # Message de non-connexion
+      mainPanel("Vous n'êtes pas connecté. Connectez-vous pour accéder à cette page.")
+    }
+  })
+}
+
+shinyApp(ui = ui, server = server)
